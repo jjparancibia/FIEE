db225db5a81670b39ff25740e5816105e6b70b2f
Jonathan Jean Paul Arancibia Sanabria <jjp.arancibia@outlook.com>
2016-12-21 13:23:49 -0500

    Create archivo9.cpp

---
+++ archivo9.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m;
+    int n = 100;
+    for(int i = 0; i < n ; i++)
+    {
+        m = 1;
+        for(int j = 0 ; j < n ; j++)
+        {
+            m = 2;
+        }
+    }
+    return 0;
+}
