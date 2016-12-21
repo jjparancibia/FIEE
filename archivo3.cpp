3feb72763bbdaaf90eb1bea21328a5c7e2d8375e
Jonathan Jean Paul Arancibia Sanabria <jjp.arancibia@outlook.com>
2016-12-21 10:59:16 -0500

    Create archivo3.cpp

---
+++ archivo3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char letra;
+    while(1)
+    {
+        cin >> letra;
+        if(letra == 'K')
+            break;
+    }
+    return 0;
+}
