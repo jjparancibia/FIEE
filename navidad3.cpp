2bc25b783dc15f04f6a74d36fb878613244561c7
Jonathan Jean Paul Arancibia Sanabria <jjp.arancibia@outlook.com>
2016-12-23 11:18:45 -0500

    Create navidad3.cpp

---
+++ navidad3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    int fib;
+    fib = fibonacci(N);
+    cout << fib << endl;
+    return 0;
+}
+int fibonacci(int N)
+{
+    if(N==1 || N==2)
+        return 1;
+    return fibonacci(N-1) + fibonacci(N-2);
+}
